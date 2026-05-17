98ff57476a8b720bb24e2df9c6f311cee79c71ac
Rishu Sondhi <rishu@Rishus-MacBook-Air.local>
2026-05-17 02:44:44 -0300

    updated site

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -5,8 +5,8 @@ export default function Education() {
         <div className="max-w-lg mx-auto p-6 shadow-md bg-indigo-100">
           <h3 className="text-2xl font-semibold text-gray-800">Diploma in Computer Programming</h3>
           <p className="text-blue-600 hover:text-blue-800 mt-2">Sheridan College </p>
-          <p className="text-blue-600 hover:text-blue-800 mt-2">Expected Graduation: April 2025</p>
-        </div>
+          <p className="text-blue-600 hover:text-blue-800 mt-2">Graduated in April 2025</p> 
+          <p className="text-blue-600 hover:text-blue-800 mt-2">GPA : 3.8/4</p>       </div>
         <div className="max-w-lg mx-auto p-6 shadow-md bg-indigo-100 mt-6">
         {/* <div className="max-w-lg mx-auto p-6 shadow-md bg-indigo-100 mt-6"> */}
           <h3 className="text-2xl font-semibold text-gray-800">High School</h3>
